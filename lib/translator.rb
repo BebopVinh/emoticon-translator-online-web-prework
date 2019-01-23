cc4d3edec45dbff1e55755107e88bee3fdd066fd
Vinh Do <vinhdo07@gmail.com>
2019-01-23 04:11:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,12 +7,10 @@ def load_library(file_path)
   file = YAML.load_file(file_path)
   emoticons = {'get_meaning' => {}, 'get_emoticon' => {}}
   file.each do |english, emotes|
-    binding.pry
     emoticons['get_meaning'][emotes.last] = english
     emoticons['get_emoticon'][emotes.first] = emotes.last
   end
   emoticons
-  binding.pry
 end
 
 def get_japanese_emoticon
