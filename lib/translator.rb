b79be8992a4bde1453e7f0617e5e3406cdf41c36
Vinh Do <vinhdo07@gmail.com>
2019-01-23 04:19:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
     if key == emoticon
       translation = value
     else
-      "Sorry, emoticon not found."
+      translation = "Sorry, emoticon not found."
     end
   end
 end
