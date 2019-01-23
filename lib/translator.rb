f88147603ef3535cf077e84d778fed4ce17c1f6f
Vinh Do <vinhdo07@gmail.com>
2019-01-23 04:05:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file_path)
   file = YAML.load_file(file_path)
   emoticons = {'get_meaning' => {}, 'get_emoticon' => {}}
   file.each do |english, emotes|
-    emoticons['get meaning'] = {emotes.last => english}
-    emoticons['get_emoticon'] = {emotes.first => emotes.last}
+    emoticons['get meaning'][emotes.last] = english
+    emoticons['get_emoticon'][emotes.first] = emotes.last
   end
   emoticons
   binding.pry
